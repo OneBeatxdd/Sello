0f9659bfeeb67f9cf27cb52135dcc539d8f21b2a
OneBeatxdd <sallyyue123xdd@gmail.com>
2019-05-25 13:59:40 +0800

    Added base and index
    test if the setup is correct

--- Sello/urls.py
+++ Sello/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # /admin/
+    url(r'^admin/', admin.site.urls),
+
+    # /
+    url(r'^$', views.index, name='index'),
 ]
