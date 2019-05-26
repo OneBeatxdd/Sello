9aee92f12cd5ca1be935463e248910dc4f913bd0
OneBeatxdd <sallyyue123xdd@gmail.com>
2019-05-26 16:20:40 +0800

    ajax request for changing card status
    When card drop will change db status

--- task/urls.py
+++ task/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.TaskDetails.as_view(), name='task-detail'),
 
     # /task/delete/70     <- task id
-    url(r'^delete/(?P<pk>[0-9]+)/$', views.TaskDelete.as_view(), name='task-delete')
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.TaskDelete.as_view(), name='task-delete'),
+
+    # /task/update/
+    url(r'^update/$', views.update_status, name='task-update')
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template import loader
 from .models import Task
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -36,3 +37,16 @@ class TaskDelete(DeleteView):
         else:
             return super(TaskDelete, self).post(request, *args, **kwargs)
 
+
+def update_status(request):
+    id = request.POST.get("card_id", default=None)
+    status = request.POST.get("status", default=None)
+    card = Task.objects.get(pk=id)
+    card.Status = status
+    card.save()
+    template = loader.get_template('task/task_list.html')
+    allTasks = Task.objects.all()
+    context = {
+        'allTasks': allTasks
+    }
+    return HttpResponse(template.render(context, request))
